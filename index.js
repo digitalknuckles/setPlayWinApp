d3f6645f0201b5b9d61138261e2900c9c926c66d
Digitalknuckles <digitalknuckles@gmail.com>
2025-04-25 10:24:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Import React and ReactDOM to render the React app
-import React from 'react';
+//import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'; // Assuming your App component is in App.js
 
