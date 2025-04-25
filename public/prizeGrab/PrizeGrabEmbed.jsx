9e8388c73ee512a9d2c63573a91284034388bc60
Digitalknuckles <digitalknuckles@gmail.com>
2025-04-25 08:56:12 -0400

    Create PrizeGrabEmbed.jsx

---
+++ public/prizeGrab/PrizeGrabEmbed.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PrizeGrabEmbed = () => (
+  <div style={{ width: '100%', height: '600px', maxWidth: '800px', margin: '0 auto' }}>
+    <iframe
+      src="/prizeGrab/index.html"
+      title="Prize Grab Game"
+      width="100%"
+      height="100%"
+      style={{ border: 'none', borderRadius: '8px' }}
+    />
+  </div>
+);
+
+export default PrizeGrabEmbed;
