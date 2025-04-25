7b7200adcebad7806cf119b6f122646cf1696c8e
Digitalknuckles <digitalknuckles@gmail.com>
2025-04-25 15:11:44 -0400

    Update game.js

--- docs/public/CyberPetsAiTrainer/game.js
+++ docs/public/CyberPetsAiTrainer/game.js
@@ -1,4 +1,4 @@
-import { mintPrize } from './public/CyberPetsAiTrainer/walletconnect.js';
+import { mintPrize } from './walletconnect.js';
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
