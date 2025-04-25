772fbb63f24f59e8e811c84fd02f05f199f3213c
Digitalknuckles <digitalknuckles@gmail.com>
2025-04-25 09:48:19 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import PrizeGrabEmbed from './components/PrizeGrabEmbed'; // Import PrizeGrabEmbed component
+
+function App() {
+  return (
+    <div className="App">
+      {/* Render the PrizeGrabEmbed component */}
+      <PrizeGrabEmbed />
+    </div>
+  );
+}
+
+export default App;
